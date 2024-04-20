b7add75f2c3ad53b87e2a95f8be8917743f999f4
Marie Blakely <marieblakely@myyahoo.com>
2024-04-20 15:07:47 -0400

    create show view, route, and controller function

--- controllers/locations.js
+++ controllers/locations.js
@@ -1,6 +1,20 @@
 import { Location } from "../models/location.js";
 
 
+function index(req, res){
+  Location.find({})
+  .then(locations => {
+    res.render('locations/index', {
+      locations: locations,
+      title: 'All Locations'
+    })
+  })
+    .catch(err => {
+      console.log(err)
+      res.redirect('/locations')
+  })
+}
+
 function newLocation (req, res){
   res.render('locations/new', {
     title: 'Add Location'
@@ -8,13 +22,39 @@ function newLocation (req, res){
 }
 
 
+function create(req, res){
+  Location.create(req.body)
+  .then(location => {
+    res.redirect('/locations')
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/locations')
+  })
+}
 
-
-
+function show(req, res) {
+  Location.findById(req.params.locationId)
+  .populate('author')
+  .populate('comments.author')
+  .then(location => {
+    res.render('locations/show', {
+      title: 'Location Details',
+      location
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/locations')
+  })
+}
 
 
 
 
 export {
+  index,
   newLocation as new,
+  create,
+  show,
 }
--- routes/locations.js
+++ routes/locations.js
@@ -5,8 +5,14 @@ import * as locationsCtrl from '../controllers/locations.js'
 const router = Router()
 
 
+router.get('/', isLoggedIn, locationsCtrl.index)
 //GET localhost:3000/locations/new
 router.get('/new', isLoggedIn, locationsCtrl.new)
+
+router.post('/', isLoggedIn, locationsCtrl.create)
+
+router.get('/:locationId', isLoggedIn, locationsCtrl.show)
+
 
 
 export {
